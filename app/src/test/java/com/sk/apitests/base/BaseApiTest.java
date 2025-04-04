3cf8ecd8f32e5085b2ce1fbb62ab673cdf2c907d
SerhiiKlymenko <mafakka@mail.ru>
2025-04-04 19:06:46 -0400

    config and config reader

--- app/src/test/java/com/sk/apitests/base/BaseApiTest.java
+++ app/src/test/java/com/sk/apitests/base/BaseApiTest.java
@@ -1,5 +1,6 @@
-package com.sk.apitests.basictests;
+package com.sk.apitests.base;
 
+import com.sk.apitests.utils.ConfigReader;
 import io.qameta.allure.testng.AllureTestNg;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -8,22 +9,26 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.number.OrderingComparison.lessThan;
 
 @Listeners({AllureTestNg.class})
-public class BasitApiTest {
+public class BaseApiTest {
+
+//    @BeforeMethod
+//    public void setUp() {
+//
+//    }
 
     @Test
     public void test_connection() {
         given()
-                .baseUri("https://jsonplaceholder.typicode.com")
+                .baseUri(ConfigReader.get("base.url"))
                 .basePath("/posts")
                 .log()
                 .body()
-                .when()
+            .when()
                 .get()
-                .then()
-                .log()
-                .body()
+            .then()
+                .log().body()
                 .statusCode(200)
-                .time(lessThan(5000L));
+                .time(lessThan(1000L));
 
     }
 }
---
+++ app/src/test/java/com/sk/apitests/base/Specs.java
@@ -0,0 +1,30 @@
+package com.sk.apitests.base;
+
+import com.sk.apitests.utils.ConfigReader;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import java.io.ObjectInputFilter;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.Matchers.lessThan;
+
+
+public class Specs {
+
+  public static RequestSpecification userRequest = new RequestSpecBuilder()
+            .setBaseUri(ConfigReader.get("base.url"))
+            .setContentType(ContentType.JSON)
+            .setBasePath("/users")
+            .build();
+
+  public static ResponseSpecification userResponse = new ResponseSpecBuilder()
+          .expectStatusCode(200)
+          .expectResponseTime(lessThan(2L), TimeUnit.SECONDS)
+          .build();
+
+
+}
