db11310e93ce0966aab23db2f9b7f8ae3c17ece0
SerhiiKlymenko <mafakka@mail.ru>
2025-04-06 16:34:25 -0400

    base test -> connection test

--- app/src/test/java/com/sk/apitests/base/ConnectionTest.java
+++ app/src/test/java/com/sk/apitests/base/ConnectionTest.java
@@ -1,34 +1,25 @@
 package com.sk.apitests.base;
 
-import com.sk.apitests.utils.ConfigReader;
 import io.qameta.allure.testng.AllureTestNg;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.number.OrderingComparison.lessThan;
 
 @Listeners({AllureTestNg.class})
-public class BaseApiTest {
-
-//    @BeforeMethod
-//    public void setUp() {
-//
-//    }
+public class ConnectionTest {
 
     @Test
     public void test_connection() {
         given()
-                .baseUri(ConfigReader.get("base.url"))
-                .basePath("/posts")
+                .spec(Specs.connectionTestRequest)
                 .log()
                 .body()
             .when()
                 .get()
             .then()
-                .log().body()
-                .statusCode(200)
-                .time(lessThan(1000L));
-
+                .spec(Specs.connectionTestResponse)
+                .log()
+                .body();
     }
 }
--- app/src/test/java/com/sk/apitests/base/Specs.java
+++ app/src/test/java/com/sk/apitests/base/Specs.java
@@ -7,7 +7,6 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
-import java.io.ObjectInputFilter;
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.Matchers.lessThan;
@@ -15,16 +14,27 @@ import static org.hamcrest.Matchers.lessThan;
 
 public class Specs {
 
-  public static RequestSpecification userRequest = new RequestSpecBuilder()
+    public static RequestSpecification connectionTestRequest = new RequestSpecBuilder()
+            .setBaseUri(ConfigReader.get("base.url"))
+            .setContentType(ContentType.JSON)
+            .setBasePath("/posts")
+            .build();
+
+    public static ResponseSpecification connectionTestResponse = new ResponseSpecBuilder()
+            .expectStatusCode(200)
+            .expectResponseTime(lessThan(2L), TimeUnit.SECONDS)
+            .build();
+
+    public static RequestSpecification userRequest = new RequestSpecBuilder()
             .setBaseUri(ConfigReader.get("base.url"))
             .setContentType(ContentType.JSON)
             .setBasePath("/users")
             .build();
 
-  public static ResponseSpecification userResponse = new ResponseSpecBuilder()
-          .expectStatusCode(200)
-          .expectResponseTime(lessThan(2L), TimeUnit.SECONDS)
-          .build();
+    public static ResponseSpecification userResponse = new ResponseSpecBuilder()
+            .expectStatusCode(200)
+            .expectResponseTime(lessThan(2L), TimeUnit.SECONDS)
+            .build();
 
 
 }
