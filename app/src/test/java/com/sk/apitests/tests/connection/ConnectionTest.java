50d19cf819a56ecb66dd9617d559f019d78e92ae
SerhiiKlymenko <mafakka@mail.ru>
2025-04-07 19:35:39 -0400

    Specs, BaseTest(superclass), Endpoints extraction, users test

--- app/src/test/java/com/sk/apitests/tests/connection/ConnectionTest.java
+++ app/src/test/java/com/sk/apitests/tests/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package com.sk.apitests.tests.connection;
 
+import com.sk.apitests.base.BaseTest;
 import com.sk.apitests.base.Specs;
 import io.qameta.allure.testng.AllureTestNg;
 import org.testng.annotations.Listeners;
@@ -8,19 +9,17 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 
 @Listeners({AllureTestNg.class})
-public class ConnectionTest {
+public class ConnectionTest extends BaseTest {
 
-    @Test
+    @Test(description = "Verify sever responds with status 200")
     public void test_connection() {
         given()
-                .spec(Specs.connectionTestRequest)
-                .log()
-                .body()
-            .when()
+                .spec(Specs.requestSpecConnectionTest)
+                .log().body()
+        .when()
                 .get()
-            .then()
-                .spec(Specs.connectionTestResponse)
-                .log()
-                .body();
+        .then()
+                .spec(Specs.responseSpecOK)
+                .log().body();
     }
 }
---
+++ app/src/test/java/com/sk/apitests/tests/users/UsersEndpointTest.java
@@ -0,0 +1,34 @@
+package com.sk.apitests.tests.users;
+
+import com.sk.apitests.base.BaseTest;
+import io.qameta.allure.testng.AllureTestNg;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import static com.sk.apitests.base.Specs.requestUserEndpoint;
+import static com.sk.apitests.base.Specs.responseSpecOK;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@Listeners({AllureTestNg.class})
+public class UsersEndpointTest extends BaseTest {
+
+    @Test(description = "Verify fetching user with ID 1 returns correct data")
+    public void testGetUserById_Success() {
+        Integer userId = 1;
+        given()
+                .spec(requestUserEndpoint)
+                .pathParam("id", userId)
+                .log().uri()
+                .log().params()
+        .when()
+                .get("/{id}")
+        .then()
+                .log().body()
+                .spec(responseSpecOK)
+                .body("id", equalTo(userId))
+                .body("email", equalTo("Sincere@april.biz"));
+
+    }
+
+}
